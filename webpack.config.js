58d13b90890814c4f9d0b3e5f565f981a2b07ae8
david <david.medragh@gmail.com>
2020-06-14 15:23:01 +0200

    migrate babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
+var webpack= require('webpack');
 module.exports = {
     entry : './script.js',
-    output : {filename : 'bundle.js'},
+    output : {
+        filename : 'bundle.js'
+    },
     module : {
         rules : [
             {test: /\.js?/, loader: 'babel-loader', exclude: /node_modules/}
